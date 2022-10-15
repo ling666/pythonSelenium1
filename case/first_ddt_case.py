83ee257dc8d87d60ee6c110731bb6e42269bb054
ling666 <760724206@qq.com>
2022-10-15 11:47:22 +0800

    excel

--- case/first_ddt_case.py
+++ case/first_ddt_case.py
@@ -9,7 +9,10 @@ from selenium import webdriver
 import unittest
 import warnings
 import HTMLTestRunner
+from util.excel_util import ExcelUtil
 
+ex = ExcelUtil()
+data = ex.get_data()
 #邮箱、用户名、密码、验证码、错误信息定位元素、错误提示信息
 @ddt.ddt
 class FirstDdtCase(unittest.TestCase):
@@ -31,13 +34,17 @@ class FirstDdtCase(unittest.TestCase):
         #print("这是case的后置条件")
         self.driver.close()
 
+    '''
     @ddt.data(
         ['88','1111','11111','111','userEmailError','请输入有效的电子邮件地址'],
         ['@qq.com', '1111', '11111', '111', 'userEmailError', '请输入有效的电子邮件地址'],
         ['88@qq.com', '1111', '11111', '111', 'userEmailError', '请输入有效的电子邮件地址']
     )
     @ddt.unpack
-    def test_register_case(self,email,username,password,code,assertCode,assertText):
+    '''
+    @ddt.data(*data) #数据是一个循环的
+    def test_register_case(self,data):   #每次拿一列数据，即data等于一列的数据
+        email, username, password, code, assertCode, assertText = data  #进行list赋值
         emailError = self.login.register_function(email,username,password,code,assertCode,assertText)
         self.assertFalse(emailError,"邮箱格式正确")
         # if emailError == True:
